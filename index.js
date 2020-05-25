3debe30efb50336815c7fad0a14b099f9b4764f9
Rafael Mayer <mayer.rafa@outlook.com>
2020-05-25 01:05:56 -0300

    Criando Rep Convert My Money

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const convert = require('./lib/convert')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/cotacao', (req, res) => {
+    const { cotacao, quantidade } = req.query
+    if (cotacao && quantidade) {
+        const conversao = convert.convert(cotacao, quantidade)
+        res.render('cotacao', {
+            cotacao: convert.toMoney(cotacao),
+            quantidade: convert.toMoney(quantidade),
+            conversao: convert.toMoney(conversao)
+        })
+    }else {
+        const conversao = convert.convert(cotacao, quantidade)
+        res.render('cotacao', {
+            cotacao: 0,
+            quantidade: 0,
+            conversao
+        })
+    }
+})
+
+app.listen(3000, err => {
+    if (err) {
+        console.log('nao foi possivel iniciar')
+    } else {
+        console.log("ConvertMyMoney is online")
+    }
+})
